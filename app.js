3000812e9acbd0482bec2e2fb0644075625a0098
Gilbert Adu <gea33@cornell.edu>
2024-11-22 06:56:03 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,154 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const axios = require("axios");
+require('dotenv').config();
+
+const stripe_key = process.env.STRIPE_SECRET_KEY
+const stripe = require('stripe')(stripe_key);
+
+
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/test", (Req, res) => {
+    res.send("frontend is working!!!")
+})
+app.get("/about", (req, res) => {
+    try {
+        res.render("about")
+
+    }catch(error) {
+        console.log("error occurred getting about page: ", error)
+    }
+
+});
+app.get("/FAQS", (req, res) => {
+    try {
+        res.render("faq")
+
+    }catch(error) {
+        console.log("error occurred getting FAQ page: ", error)
+    }
+
+}); 
+app.get('/', async(req, res) => {
+    try {
+        const all_posts = await axios.get("http://localhost:5001/");
+        //console.log(all_posts.data);
+        //
+
+        res.render('index', {posts: all_posts.data});
+        //res.render('index', {posts: []});
+
+
+
+    }catch(error) {
+        console.error('Error fetching all post data: ', error)
+
+    }
+});
+
+app.get('/view-post', async (req, res) => {
+    try{
+        const { post_id } = req.query;
+        //console.log('post_id: ', post_id);
+        const post_details = await axios.get('http://localhost:5001/post-details', {
+            params: {
+                post_id: post_id
+            }
+        })
+        //console.log(post_details)
+        res.render('post', {post_details: post_details})
+    
+
+    }catch(error){
+        console.log('error fetching view post: ', error)
+    }
+});
+
+
+app.get('/get-submission-info', (req, res) => {
+
+
+});
+app.get('/stripe', async(req, res) => {
+
+    //const total = parseInt(req.body.price) * parseInt(req.body.quantity) ;
+    //const submission_details = await axios.get('http://localhost:3000/get-submission-info');
+    let total = 49;
+
+    const { report_type, videoUpload } = req.query;
+    //console.log("report type is: ", report_type)
+
+    if (videoUpload == 'yes') {
+        total += 29;
+    }
+    
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total * 100,
+        currency: 'usd',
+        automatic_payment_methods: {
+            enabled: true
+        }
+    })
+    res.render('stripe', {client_secret: paymentIntent.client_secret})
+
+
+    //res.render('stripe');
+})
+
+
+//pay to vote
+app.get('/pay-to-vote-stripe', async(req, res) => {
+
+    //const total = parseInt(req.body.price) * parseInt(req.body.quantity) ;
+    const total = 49
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total * 0.01,
+        currency: 'usd',
+        automatic_payment_methods: {
+            enabled: true
+        }
+    })
+    res.render('stripe', {client_secret: paymentIntent.client_secret})
+
+
+    //res.render('stripe');
+})
+
+
+app.get('/confirm-payment', (req, res) => {
+    res.render('confirmation');
+});
+
+const storage = multer.memoryStorage(); // or configure for file storage
+const upload = multer({ storage: storage });
+
+app.post('/create-post', upload.single('media'), (req, res) => {
+console.log('create-post-endpoint: ', req.body);
+const fileData = req.file;
+console.log('fileData: ', fileData);
+    res.json({
+        message: 'Form data received successfully',
+        data: req.body
+    })
+
+})
+
+app.get("/search/:query", (req, res) => {
+    const query = req.params.query;
+    console.log("query is: ", query);
+    res.status(200).send("query is sent");
+});
+app.listen(port, () => {
+    console.log('up on 3000')
+})
---
+++ public/index.js
@@ -0,0 +1,315 @@
+const form = document.getElementById("progressiveForm");
+const steps = Array.from(document.querySelectorAll(".form-step"));
+const progress = document.getElementById("progress");
+let currentStep = 0;
+
+
+const fileToBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+  };
+// Function to update progress bar and buttons
+let formData = {};
+
+async function submitForm() {
+
+    //1. go to payment page instead in the next step function
+
+
+
+    const videoInput = document.getElementById('videoInput');
+    const imageInput = document.getElementById('imageInput');
+
+    //for image submission
+    if (imageInput && imageInput.files) {
+        const imageFile = document.getElementById('imageInput').files[0];
+
+        if (imageFile) {
+            const formimageData = new FormData();
+            formimageData.append('file', imageFile);
+
+            const response = await fetch('http://localhost:5001/upload', {
+                method: 'POST',
+                body: formimageData
+            });
+            const data = await response.json();
+            console.log("data: ", data);
+            if (response.ok) {
+                console.log("photo id is: ", data.fileUrl)
+                //formData["photo_id"] = data.fileUrl;
+
+                //use set photo id here
+                sessionStorage.setItem('photo_id', JSON.stringify(data.fileUrl));
+
+                alert(`image url in vanilla js is: ${data.fileUrl}`)
+            }else {
+                alert(`Failed to upload image: ${data.error}`)
+
+            }
+        }
+    }
+
+
+    //for video submission
+    if (videoInput && videoInput.files ){
+        const videoFile = document.getElementById('videoInput').files[0];
+
+        if (videoFile) {
+            //console.log("video: ", videoFile);
+            formData['mediaType'] = videoFile.type
+            //formData['video'] = await fileToBase64(videoFile);
+            const formvideoData = new FormData();
+            formvideoData.append('file', videoFile);
+
+            const response = await fetch('http://localhost:5001/upload', {
+                method: 'POST',
+                body: formvideoData
+            });
+            const data = await response.json();
+            if (response.ok) {
+                formData['file_url'] = data.fileUrl;
+                //console.log('data: ', data)
+                //console.log("file_url: ", data.fileUrl)
+                sessionStorage.setItem('video_url', JSON.stringify(data.fileUrl));
+
+                alert(`file url in vanilla js is: ${data.fileUrl}`)
+                //console.log('file url is: ', data['fileUrl'])
+            }else {
+                alert(`Failed to upload file: ${data.error}`)
+            }
+            /**
+             * const reader = new FileReader();
+            reader.readAsDataURL(videoFile);
+            reader.onload = async function () {
+                const base64video = reader.result
+                console.log('base64video: ', base64video)
+                formData['video'] = base64video;
+
+            }
+
+             */
+            
+        }
+    }else {
+        formData['video'] = {};
+
+
+    }
+
+    
+
+    
+
+    try {
+        
+        /**
+         * const response = await fetch('http://localhost:5001/create-report', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+        
+        const data = await response.json();
+        
+        console.log('Response from python backend: ', data)
+        
+
+         */
+        console.log(formData);
+        document.getElementById('loading-overlay').style.display = 'block';
+        setTimeout(() => {
+            document.getElementById('loading-overlay').style.display = 'none';
+            //window.location.href="/"
+        }, 10000)
+
+        setTimeout(() => {
+            let report_type = sessionStorage.getItem('report_type');
+            let videoUpload = sessionStorage.getItem('videoUpload');
+
+            
+            const data = {'report_type': report_type, 'videoUpload': videoUpload}
+            const queryString = new URLSearchParams(data).toString();
+            window.location.href=`http://localhost:3000/stripe?${queryString}`
+        }, 2000)
+
+
+    }catch(error) {
+        console.error("Error sending data: ", error);
+    }
+
+
+};
+
+
+
+function updateProgress() {
+    const progressPercentage = ((currentStep + 1) / steps.length) * 100;
+    progress.style.width = `${progressPercentage}%`;
+
+    document.getElementById("backBtn").style.display = currentStep > 0 ? "inline-block" : "none";
+    document.getElementById("nextBtn").textContent = currentStep === steps.length - 1 ? "Proceed to Payment" : "Next";
+
+    /**
+     * if (document.getElementById("nextBtn").textContent == 'Submit') {
+        document.getElementById("nextBtn").type = "submit";
+    }
+     */
+}
+
+function getCurrentStep() {
+    const currentStep = document.querySelector('.form-step.active');
+    const currentStepAttr = currentStep.getAttribute('data-step');
+    const modalHeader = document.getElementById('modal-header');
+    if (parseInt(currentStepAttr, 10) == 12) {
+        document.getElementById('subject_address').style.display = 'block';
+
+    }else if (parseInt(currentStepAttr, 10) > 12) {
+        document.getElementById('subject_address').style.display = 'none';
+    }
+
+    if (parseInt(currentStepAttr, 10) == 10) {
+        const imageInputLabel = document.querySelector('.image-upload-container');
+        imageInputLabel.style.display = 'block';
+    }else if (parseInt(currentStepAttr, 10) > 10) {
+        //hide image uploaded
+        const imageInputLabel = document.querySelector('.image-upload-container');
+        imageInputLabel.style.display = 'none';
+
+
+    }
+    if (parseInt(currentStepAttr, 10) >= 11 && parseInt(currentStepAttr, 10) <= 17) {
+        modalHeader.innerText = 'Subject Info';
+        modalHeader.style.color = 'red';
+
+    }
+
+    if (parseInt(currentStepAttr, 10) >= 18 && parseInt(currentStepAttr, 10) <= 25) {
+        modalHeader.innerText = 'Incident Info';
+        modalHeader.style.color = '#0D92F4';
+
+    }
+
+    if (parseInt(currentStepAttr, 10) > 25) {
+        modalHeader.innerText = 'Report Info';
+        modalHeader.style.color = 'green';
+
+    }
+
+    
+}
+
+// Function to go to the next step
+async function nextStep() {
+    const input =  steps[currentStep].querySelector("input") || steps[currentStep].querySelector("select");
+    if (!input.checkValidity()) {
+        input.reportValidity();
+        return;
+    }
+    
+    steps[currentStep].classList.remove("active");
+    currentStep++;
+    if (input.type == 'radio') {
+        const radios = document.querySelectorAll(`input[name=${input.name}]`)
+        for (i = 0; i < radios.length; i++) {
+            if (radios[i].checked) {
+                formData[radios[i].name] = radios[i].value;
+
+                if (radios[i].name == 'altReport'){
+                    formData['alt_reporter_name'] = document.getElementById('alt_reporter_input').value;
+                    document.getElementById('alt_report_name').style.display = 'none';
+                }
+                if (radios[i].name == 'videoUpload'){
+                        document.getElementById('upload-container').style.display = 'none';     
+                }
+
+                if (radios[i].name == 'address_question') {
+                    formData['subject_address'] = document.getElementById('subject_address').value;
+                }
+            }
+        }
+        
+    }else {
+        
+        formData[input.name] = input.value;
+        if (input.name == 'report_type') {
+            sessionStorage.setItem('report_type', input.value);
+        }
+        if (input.name == 'videoUpload') {
+            sessionStorage.setItem('videoUpload', input.value);
+        }
+        
+
+        
+    }
+
+
+    if (currentStep >= steps.length) {
+        //form.submit();
+        
+
+        //console.log("submitting form...")
+        //1. go to the payments page instead
+        //2. while holding the entered post data. 
+        //3. send the held data to the python backend temporarily
+        submitForm();
+        sessionStorage.setItem('userData', JSON.stringify(formData));
+
+        
+        
+        //form.submit();
+
+
+        //return;
+    }
+
+    steps[currentStep].classList.add("active");
+    updateProgress();
+    getCurrentStep();
+    
+}
+
+// Function to go to the previous step
+function previousStep() {
+    steps[currentStep].classList.remove("active");
+    currentStep--;
+    steps[currentStep].classList.add("active");
+    const input =  steps[currentStep].querySelectorAll("input[name=videoUpload]") || steps[currentStep].querySelector("select");
+    for (i = 0; i < input.length; i++) {
+        if (input[i].name != 'videoUpload') {
+            document.getElementById('upload-container').style.display = 'none';
+
+
+        }
+    }
+    if (input.type != 'radio')
+    updateProgress();
+    getCurrentStep();
+
+}
+// Initialize form
+updateProgress();
+
+document.getElementById('scan').addEventListener('click', () => {
+    document.getElementById('search-container').style.display = 'block';
+});
+
+const searchInput = document.getElementById('searchInput');
+searchInput.addEventListener('keypress', (event) => {
+
+    if (event.key === 'Enter') {
+        const inputValue = searchInput.value;
+        console.log('input value: ', inputValue);
+        searchInput.value = '';
+        window.location.href = `http://localhost:3000/search/${inputValue}`;
+
+    }
+});
+
+
+
